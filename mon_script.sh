7a5079b0a53a5abbece114798458754d633d8df9
EddinAdou <142503187+EddinAdou@users.noreply.github.com>
2024-03-29 13:49:18 +0100

    Update mon_script.sh

--- mon_script.sh
+++ mon_script.sh
@@ -1,88 +1,148 @@
 #!/bin/bash
 
+if [ "$#" -lt 1 ] || ([ "$1" != "user" ] && [  "$1" != "install" ] && [ "$1" != "configure_site" ] && [ "$1" != "active_site" ]); then
+        echo "Usage: $0 user <username> <password> or $0 install or $0 configure_site <site_name> <http_port> or $0 active_site <site_name>"
+        exit 1
+fi
+
+create_user() {
+        if id -u "$1" >/dev/null 2>&1; then
+                echo "L'utilisateur '$1' existe deja,"
+                return 1
+        fi
+        useradd -m -s /bin/bash "$1"
+        if [ "$?" -ne 0 ]; then
+                echo "Impossible de créer l'utilisateur '$1'."
+                return 1
+        fi
+
+        echo "L'utilisateur '$1' a ete créé avec succès!"
+        return 0
+}
+
+add_password() {
+        if ! id -u "$1" >/dev/null 2>&1; then
+                echo "L'utilisateur $1 n'existe pas!"
+                return 1
+        fi
+
+        local password="$2"
+        echo "$1:$password" | chpasswd
+
+        if [ "$?" -ne 0 ]; then
+                echo "Impossible de définir le password de l'utilisateur $1"
+                return 1
+        fi
+        echo "Mot de passe de $1 créé avec succès!"
+        return 0
+}
+
+install_nginx() {
+        apt update
+        apt install -y nginx
+
+        if [ "$?" -ne 0 ]; then
+                echo "Impossible d'installer nginx"
+                return 1
+        fi
+        echo "nginx a été installé avec succès!"
+        return 0
+}
+
 configure_site() {
-  if [ -z "$1" ] || [ -z "$2" ]; then
-    echo "Erreur : veuillez spécifier un nom pour le site à configurer et le port HTTP."
-    return 1
-  fi
+        if [ "$#" -ne 2 ]; then
+                echo "Usage: $0 configure_site <site_name> <http_port> "
+                return 1
+        fi
+	site_name="$1"
+	http_port="$2"
 
-  config_file="/etc/nginx/sites-available/$1"
-  web_root="/var/www/$1"
+        config="/etc/nginx/sites-available/default"
+        new_config="/etc/nginx/sites-available/$site_name"
+	web_root="/var/www/$site_name"
 
-  if [ -e "$config_file" ]; then
-    echo "Erreur : un fichier de configuration pour $1 existe déjà."
-    return 1
-  fi
+	mkdir -p "$web_root"
+	if [ "$?" -ne 0 ]; then
+		echo "Impossible de créer le repertoire pour $site_name"
+		return 1
+	fi
+	echo "<html><head><title>$site_name</title></head><body><h1>$site_name</h1></body></html>" > "$web_root/index.html"
+	if [ "$?" -ne 0 ]; then
+	echo "Impossible de créer le fichier index.html pour $site_name"
+	return 1
+	fi
 
-  if [ -d "$web_root" ]; then
-    echo "Erreur : le répertoire $web_root existe déjà."
-    return 1
-  fi
+	cp "$config" "$new_config"
+	if [ "$?" -ne 0 ]; then
+	echo "Impossuble de créer le fichier de config pour $site_name"
+	return 1
+	fi
 
-  mkdir -p "$web_root"
-  echo "<html><head><title>$1</title></head><body><h1>$1</h1></body></html>" > "$web_root/index.html"
+	sed -i "s/server_name .*/server_name $site_name;/" "$new_config"
+	sed -i "s/listen [[:digit:]]\+ default_server/listen $http_port default_server;/" "$new_config"
+	sed -i "s/listen \[::]:[[:digit:]]\+ default_server/listen [::]:$http_port default_server;/" "$new_config"
+	sed -i "s/root .*/root $web_root;/" "$new_config"
+        echo " fichier config de $1 a été créé avec succès : $new_config"
 
-  cp /etc/nginx/sites-available/default "$config_file"
-
-  sed -i "s/server_name _;/server_name $1;/" "$config_file"
-  sed -i "s/listen 80 default_server;*/listen $2 default_server; listen [::]:$2 default_server;/" "$config_file"
-  sed -i "s#root /var/www/html;#root $web_root;#" "$config_file"
-
-  echo "Fichier de configuration créé pour $1 sur le port $2."
-  echo "Répertoire $web_root créé avec le fichier index.html."
-
+	echo "Fichier config de $site_name créé avec succès : $new_config"
+	echo "Fichier index.html crée avec succes: $web_root/index.html"
+        return 0
 }
+
 active_site() {
-  if [ -z "$1" ]; then
-    echo "Erreur : veuillez entrer un nom de site à activer."
-    return 1
-  fi
+	if [ "$#" -ne 1 ]; then
+		echo "Usage: $0 active_site <site_name>"
+		return 1
+	fi
+	site_name="$1"
+	config_file="/etc/nginx/sites-available/$site_name"
+	enabled_link="/etc/nginx/sites-enabled/$site_name"
 
-  config_file="/etc/nginx/sites-available/$1"
+	nginx -t -c "/etc/nginx/nginx.conf" >/dev/null 2>&1
+	if [ "$?" -ne 0 ]; then
+		echo "La syntaxe de la config est incorrect pour $site_name"
+		return 1
+	fi
+	ln -sf "$config_file" "$enabled_link"
+	if [ "$?" -ne 0 ]; then
+		echo "Impossible de creer le lien symbolique pour activer $site_name"
+		return 1
+	fi
 
-  if [ ! -e "$config_file" ]; then
-    echo "Erreur : aucun fichier de configuration pour $1 n'a été trouvé."
-    return 1
-  fi
+	systemctl reload nginx
+	if [ "$?" -ne 0 ]; then
+		echo "Impossible de recharger nginx"
+		return 1
+	fi
+	echo "$site_name a éte créé avec succès"
+	return 0
 
-  nginx -t -c "$config_file" >/dev/null 2>&1
-
-  if [ $? -ne 0 ]; then
-    echo "Erreur : la syntaxe du fichier de configuration est invalide."
-    return 1
-  fi
-
-  ln -s "$config_file" "/etc/nginx/sites-enabled/$1"
-  systemctl reload nginx
-
-  echo "La configuration de $1 a été activée et le service Nginx a été rechargé."
 }
 
-if [ $1 = "install" ]; then
-    echo "Mise à jour des dépôts et installation du paquet nginx..."
-    apt update
-    apt install -y nginx
-    exit 0
-fi
-
-if [ $1 = "configure_site" ] && [ $# -eq 3 ]; then
-    configure_site $2 $3
-    exit 0
-fi
-
-if [ $# -ne 3 ]; then
-    echo "Usage: $0 <user|configure_site> <username|site_name> <password|http_port>"
+if [ "$1" = "user" ]; then
+  if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 user <username> <password>"
     exit 1
+  fi
+  create_user "$2"
+  add_password "$2" "$3"
+elif [ "$1" = "install" ]; then
+  install_nginx
+elif [ "$1" = "configure_site" ]; then
+  if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 configure_site <site_name> <http_port>"
+    exit 1
+  fi
+  shift
+  configure_site "$@"
+elif [ "$1" = "active_site" ]; then
+  if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 active_site <site_name>"
+    exit 1
+  fi
+  shift
+  active_site "$@"
+else
+  echo "Usage: $0 user <username> <password> or $0 install or $0 configure_site <site_name> <http_port> or $0 active_site <site_name>"
+  exit 1
 fi
-
-user=$1
-username=$2
-password=$3
-
-useradd -m $username
-echo "$username:$password" | sudo chpasswd
-
-echo "Utilisateur $username créé avec succès."
-echo "Nom d'utilisateur: $username"
-echo "Mot de passe : $password"
-
